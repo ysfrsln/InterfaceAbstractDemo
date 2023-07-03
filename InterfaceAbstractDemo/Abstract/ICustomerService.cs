c573957736842d8a961a82e1aaa9cd11a5c63f20
yusuf <yusuferaslan@hotmail.com>
2023-07-04 00:31:15 +0300

    Add project files.

---
+++ InterfaceAbstractDemo/Abstract/ICustomerService.cs
@@ -0,0 +1,17 @@
+﻿using InterfaceAbstractDemo.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceAbstractDemo.Abstract
+{
+    internal interface ICustomerService
+    {
+        void Save(Customer customer);
+
+    }
+
+    
+}
---
+++ InterfaceAbstractDemo/Program.cs
@@ -0,0 +1,24 @@
+﻿using InterfaceAbstractDemo.Abstract;
+using InterfaceAbstractDemo.Adapters;
+using InterfaceAbstractDemo.Concrete;
+using InterfaceAbstractDemo.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceAbstractDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseCustomerManager customerManager = new StarbucksCustomerManager(new MernisServiceAdapter());
+            customerManager.Save(new Customer { DateOfBirth = new DateTime(1989, 2, 23), FirstName = "Yusuf", LastName = "Eraslan", NationalityId = "0123456789" });
+
+            Console.ReadLine();
+        }
+
+    }
+}
